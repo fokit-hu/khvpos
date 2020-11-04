2b721a73d415f68fba0b519344c588f0b4fe9087
Karoly Gossler <connor@connor.hu>
2020-11-04 11:29:48 +0100

    proper namespace for tests

--- tests/PaymentGateway/PaymentRequestArgumentsTest.php
+++ tests/PaymentGateway/PaymentRequestArgumentsTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace PaymentGatewayTests;
+namespace KHBankTools\Tests\PaymentGateway;
 
 use KHBankTools\PaymentGateway\PaymentRequestArguments;
 use PHPUnit\Framework\TestCase;
