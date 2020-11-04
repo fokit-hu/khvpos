7d2113b240dd623dc5a2172d741f1f3f163e247c
Karoly Gossler <connor@connor.hu>
2020-11-04 08:45:26 +0100

    added host/endpoint tests

---
+++ tests/PaymentGatewayTest.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+use KHBankTools\PaymentGateway\PaymentGateway;
+use KHBankTools\PaymentGateway\SignatureProvider;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpClient\Psr18Client;
+
+class PaymentGatewayTest extends TestCase
+{
+    private function getAccessibleMethod(string $className, string $method)
+    {
+        $paymentGateway = new ReflectionClass($className);
+        $method = $paymentGateway->getMethod($method);
+        $method->setAccessible(true);
+        
+        return $method;
+    }
+        
+    public function testHost(): void
+    {
+        $httpClient = new Psr18Client();
+        $signatureProvider = new SignatureProvider(__DIR__.'/test_private_key.pem');
+        $publicGetHostMethod = $this->getAccessibleMethod(PaymentGateway::class, 'getEndpointBase');
+
+        $paymentGateway = new PaymentGateway(1234, $signatureProvider, true);
+        $this->assertSame($publicGetHostMethod->invokeArgs($paymentGateway, []), 'https://pay.sandbox.khpos.hu/pay/v1');
+
+        $paymentGateway = new PaymentGateway(1234, $signatureProvider, false);
+        $this->assertSame($publicGetHostMethod->invokeArgs($paymentGateway, []), 'https://pay.khpos.hu/pay/v1');
+    }
+}
