7ef873c2cf6c9a5092948eb8c609fd3b3a857943
Karoly Gossler <connor@connor.hu>
2024-02-14 14:09:21 +0100

    added more key tests (#11)

--- tests/Tests/Keys/PrivateKeyTest.php
+++ tests/Tests/Keys/PrivateKeyTest.php
@@ -9,13 +9,13 @@ use PHPUnit\Framework\TestCase;
 
 class PrivateKeyTest extends TestCase
 {
-    public function testNotLoadedByDefault()
+    public function testNotLoadedByDefault(): void
     {
         $key = new PrivateKey('not-exitst');
         $this->assertFalse($key->isKeyLoaded());
     }
 
-    public function testMissingKey()
+    public function testMissingKey(): void
     {
         $this->expectException(KeyNotFoundException::class);
         $this->expectExceptionMessage('Key not found at "not-exitst"');
@@ -23,7 +23,7 @@ class PrivateKeyTest extends TestCase
         $key->load();
     }
 
-    public function testLoadFailed()
+    public function testLoadFailed(): void
     {
         $this->expectException(KeyNotReadableException::class);
         $this->expectExceptionMessageMatches('/Failed to read private key. OpenSSL error: ".+"\./');
@@ -31,11 +31,24 @@ class PrivateKeyTest extends TestCase
         $key->load();
     }
 
-    public function testSuccessLoad()
+    public function testSuccessLoad(): void
     {
         $key = new PrivateKey(__DIR__.'/../Fixtures/test1_private_key.pem');
         $key->load();
         $this->assertTrue($key->isKeyLoaded());
         $this->assertInstanceOf(\OpenSSLAsymmetricKey::class, $key->getSSLKey());
+    }
+
+    public function testDontLoadTwice(): void
+    {
+        $key = new PrivateKey(__DIR__.'/../Fixtures/test1_private_key.pem');
+        $key->load();
+        $objectId = spl_object_id($key->getSSLKey());
+        $key->load();
+        $this->assertSame($objectId, spl_object_id($key->getSSLKey()));
+
+        $key = new PrivateKey(__DIR__.'/../Fixtures/test1_private_key.pem');
+        $objectId = spl_object_id($key->getSSLKey());
+        $this->assertSame($objectId, spl_object_id($key->getSSLKey()));
     }
 }
--- tests/Tests/Keys/PublicKeyTest.php
+++ tests/Tests/Keys/PublicKeyTest.php
@@ -9,13 +9,13 @@ use PHPUnit\Framework\TestCase;
 
 class PublicKeyTest extends TestCase
 {
-    public function testNotLoadedByDefault()
+    public function testNotLoadedByDefault(): void
     {
         $key = new PublicKey('not-exitst');
         $this->assertFalse($key->isKeyLoaded());
     }
 
-    public function testMissingKey()
+    public function testMissingKey(): void
     {
         $this->expectException(KeyNotFoundException::class);
         $this->expectExceptionMessage('Key not found at "not-exitst"');
@@ -23,7 +23,7 @@ class PublicKeyTest extends TestCase
         $key->load();
     }
 
-    public function testLoadFailed()
+    public function testLoadFailed(): void
     {
         $this->expectException(KeyNotReadableException::class);
         $this->expectExceptionMessageMatches('/Failed to read private key. OpenSSL error: ".+"\./');
@@ -31,11 +31,24 @@ class PublicKeyTest extends TestCase
         $key->load();
     }
 
-    public function testSuccessLoad()
+    public function testSuccessLoad(): void
     {
         $key = new PublicKey(__DIR__.'/../Fixtures/test1_public_key.pem');
         $key->load();
         $this->assertTrue($key->isKeyLoaded());
         $this->assertInstanceOf(\OpenSSLAsymmetricKey::class, $key->getSSLKey());
+    }
+
+    public function testDontLoadTwice(): void
+    {
+        $key = new PublicKey(__DIR__.'/../Fixtures/test1_public_key.pem');
+        $key->load();
+        $objectId = spl_object_id($key->getSSLKey());
+        $key->load();
+        $this->assertSame($objectId, spl_object_id($key->getSSLKey()));
+
+        $key = new PublicKey(__DIR__.'/../Fixtures/test1_public_key.pem');
+        $objectId = spl_object_id($key->getSSLKey());
+        $this->assertSame($objectId, spl_object_id($key->getSSLKey()));
     }
 }
